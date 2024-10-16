21922ca32a628bae0091e7118174bba68504d5db
yaksies <samyakghising@gmail.com>
2024-10-16 16:10:08 -0400

    Creating an animated short for ICS3UI

---
+++ animatedShort.py
@@ -0,0 +1,18 @@
+'''
+                ICS3UI ANIMATED SHORT
+
+Background scenery must be detailed and diverse - /5 Points
+
+        Must Contain Linear Motion - /5 Points
+      Must Contain Non-linear motion - /5 Points
+
+Must move object made up of more than 1 shape - /5 Points
+
+  Must have multiple phases (2 or more loops) - /5 Points
+
+     Must be creative and entertaining - /5 Points
+'''
+
+
+
+
